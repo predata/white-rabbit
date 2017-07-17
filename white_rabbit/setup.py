5b59fb1c12ae4b25ff65b16fbaa20163b92ed135
Collin Stedman <collin@predata.com>
2017-07-17 11:13:35 -0400

    Add a setup.py

---
+++ white_rabbit/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name="white_rabbit",
+      version="0.1",
+      description="Compute summary stats for ALICE",
+      url="https://github.com/predata/shared/white_rabbit",
+     )
