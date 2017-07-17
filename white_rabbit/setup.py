a27af006316643f24daa2dc9bd34f4e0c2deb8e3
Collin Stedman <collin@predata.com>
2017-07-17 19:28:51 -0400

    Fix setup.py to list packages

--- white_rabbit/setup.py
+++ white_rabbit/setup.py
@@ -1,7 +1,8 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name="white_rabbit",
       version="0.1",
       description="Compute summary stats for ALICE",
       url="https://github.com/predata/shared/white_rabbit",
+      packages=find_packages(),
      )
