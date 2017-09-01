c03619dc6a2a1a90f1aa72e81fe83595c9ecaeda
David Zhao <david@predata.com>
2017-09-01 10:26:13 -0400

    Add vectorized pnl functions
    Summary:
    Fix overlapping trades filter
    
    Calc daily and cumulative returns
    
    Update docstrings
    
    Test Plan: Patch D3513 and check the results
    
    Reviewers: steve, collin
    
    Reviewed By: collin
    
    Subscribers: roly
    
    Differential Revision: https://phabricator.predata.com/D3512

--- white_rabbit/vectorized_backtest.py
+++ white_rabbit/vectorized_backtest.py
@@ -2,6 +2,8 @@ import numpy as np
 import pandas as pd
 from pandas.tseries.offsets import BDay
 
+# date + (0 * BDay()) makes date the next business day if necessary
+# this is a no-op if date is already a business day
 
 def _build_asset_df(asset_df):
     ret_df = asset_df.copy()
@@ -19,14 +21,24 @@ def _get_signal_exceedance_dates(signal_df, value_accessor, alpha, rolling_windo
     z_scores = (signal_df - signal_df_rolling.mean()) / signal_df_rolling.std()
     exceedances = z_scores.loc[z_scores[value_accessor] > alpha]
     exceedance_dates = exceedances.index
-    if ignore_overlapping:
-        exceedance_dates = exceedance_dates[~(exceedance_dates.to_series().diff() < pd.Timedelta(days=holding_period))]
     trade_dates = exceedance_dates + BDay()
     close_dates = trade_dates + pd.DateOffset(days=holding_period) + (0 * BDay())
 
+    if ignore_overlapping:
+        open_trade = pd.Series(True, trade_dates)
+        close_dates_lookup = pd.Series(close_dates, trade_dates)
+        close_dates_lookup = close_dates_lookup[~close_dates_lookup.index.duplicated()]
+        for date in trade_dates:
+            if not open_trade[~open_trade.index.duplicated()].loc[date]:
+                continue
+            open_trade.loc[date + pd.DateOffset(1):close_dates_lookup.loc[date]] = False
+        trade_dates = open_trade[open_trade].index
+        close_dates = trade_dates + pd.DateOffset(days=holding_period) + (0 * BDay())
+        exceedance_dates = trade_dates - BDay()
     return {
         "open_dates": trade_dates,
-        "close_dates": close_dates
+        "close_dates": close_dates,
+        "trigger_dates": exceedance_dates,
     }
 
 
@@ -49,14 +61,27 @@ def _get_trade_returns(asset_df, holding_period, trade_dates, is_buy):
     }
 
 
-def get_trade_statistics(signal_df, value_accessor, asset_df, alpha, rolling_window,
-                         holding_period, ignore_overlapping, is_buy):
+def _get_cumulative_returns(open_pos, close_pos, prices):
+    # value of open positions + value of closed positions
+    return ((open_pos - close_pos).cumsum() * prices + (prices * (close_pos - open_pos)).cumsum())
+
+
+def _get_daily_returns(cumulative_returns):
+    daily_returns = cumulative_returns.apply(np.log1p).diff().apply(np.exp) - 1
+    daily_returns[0] = 0
+    return daily_returns
+
+
+def get_trade_statistics(signal_df, value_accessor, asset_df, alpha, rolling_window, holding_period,
+                         ignore_overlapping, is_buy):
     """
     Calculate number of trades, hit rate, and mean return of trade returns
 
     signal_df: DataFrame of a signal's values with a DatetimeIndex and 1 column of values
     value_accessor: Column which contains the data for the provided signal_df
     asset_df: DataFrame with a DatetimeIndex and 1 column named "price"
+    alpha: Float, number of standard deviations for Bollinger bands
+    rolling_window: Integer, days of moving average lookback for Bollinger bands
     holding_period: Integer, days to hold position
     ignore_overlapping: Boolean, ignore overlapping trade triggers
     is_buy: Boolean, action is buy (True) or sell (False)
@@ -107,6 +132,8 @@ def get_trade_statistics(signal_df, value_accessor, asset_df, alpha, rolling_win
 
     trade_details = pd.concat([trade_returns_df, open_price_df, close_price_df], axis=1)
     trade_details["date"] = trade_details.index.date
+    trade_details["close_date"] = exceedances["close_dates"].date
+    trade_details["trigger_date"] = exceedances["trigger_dates"].strftime("%a %Y-%m-%d")
     trade_details = trade_details.to_dict("records")
 
     return {
@@ -115,14 +142,16 @@ def get_trade_statistics(signal_df, value_accessor, asset_df, alpha, rolling_win
     }
 
 
-def get_trade_percentiles(signal_df, value_accessor, asset_df, alpha, rolling_window,
-                          holding_periods, ignore_overlapping, is_buy, percentiles):
+def get_trade_percentiles(signal_df, value_accessor, asset_df, alpha, rolling_window, holding_periods,
+                          ignore_overlapping, is_buy, percentiles):
     """
     Calculate percentiles of trade returns
 
     signal_df: DataFrame of a signal's values with a DatetimeIndex and 1 column of values
     value_accessor: Column which contains the data for the provided signal_df
     asset_df: DataFrame with a DatetimeIndex and 1 column named "price"
+    alpha: Float, number of standard deviations for Bollinger bands
+    rolling_window: Integer, days of moving average lookback for Bollinger bands
     holding_periods: List of holding periods
     ignore_overlapping: Boolean, ignore overlapping trade triggers
     is_buy: Boolean, action is buy (True) or sell (False)
@@ -141,3 +170,45 @@ def get_trade_percentiles(signal_df, value_accessor, asset_df, alpha, rolling_wi
         quantile_values.index = (quantile_values.index * 100).astype("int")
         dist[holding_period] = quantile_values.to_dict()
     return dist
+
+
+def get_trade_returns(signal_df, value_accessor, asset_df, alpha, rolling_window, holding_period,
+                           ignore_overlapping, is_buy):
+    """
+    Calculate cumulative algo returns, daily algo returns, and cumulative benchmark returns
+
+    signal_df: DataFrame of a signal's values with a DatetimeIndex and 1 column of values
+    value_accessor: Column which contains the data for the provided signal_df
+    asset_df: DataFrame with a DatetimeIndex and 1 column named "price"
+    alpha: Float, number of standard deviations for Bollinger bands
+    rolling_window: Integer, days of moving average lookback for Bollinger bands
+    holding_periods: List of holding periods
+    ignore_overlapping: Boolean, ignore overlapping trade triggers
+    is_buy: Boolean, action is buy (True) or sell (False)
+    """
+    exceedances = _get_signal_exceedance_dates(signal_df, value_accessor, alpha, rolling_window, holding_period,
+                                               ignore_overlapping)
+    mod_asset_df = _build_asset_df(asset_df)
+
+    open_trade = (exceedances["open_dates"].value_counts() / mod_asset_df["price"]).dropna()
+    close_trade = open_trade.copy()
+    close_trade.index = close_trade.index + pd.DateOffset(days=holding_period) + (0 * BDay())
+    close_trade = close_trade.groupby(close_trade.index).sum().reindex(mod_asset_df.index, fill_value=0)
+    open_trade = open_trade.reindex(mod_asset_df.index, fill_value=0)
+
+    cumulative_returns = _get_cumulative_returns(open_trade, close_trade, mod_asset_df["price"])
+    daily_returns = _get_daily_returns(cumulative_returns)
+
+    first_date = mod_asset_df.index.min()
+    last_date = mod_asset_df.index.max()
+    start_price = mod_asset_df["price"].loc[first_date]
+    benchmark_open_trade = pd.Series(1 / start_price, [first_date])
+    benchmark_close_trade = pd.Series(benchmark_open_trade.values, [last_date]).reindex(mod_asset_df.index, fill_value=0)
+    benchmark_open_trade = benchmark_open_trade.reindex(mod_asset_df.index, fill_value=0)
+    benchmark_returns = _get_cumulative_returns(benchmark_open_trade, benchmark_close_trade, mod_asset_df["price"])
+
+    return {
+        "cumulative_returns": cumulative_returns,
+        "daily_returns": daily_returns,
+        "benchmark_returns": benchmark_returns,
+    }
