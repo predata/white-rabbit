c34c75993d1c96fde504f92c613f8d5041017891
David Zhao <david@predata.com>
2017-11-09 17:35:57 -0500

    Add unit tests to white-rabbit
    Summary:
    As above
    
    Is there a better way to store/update the constants.py?
    
    Test Plan: `python -m unittest discover`
    
    Reviewers: collin, steve
    
    Reviewed By: collin
    
    Differential Revision: https://phabricator.predata.com/D3677

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name="white_rabbit",
-      version="0.0.5",
+      version="0.0.7",
       description="Vectorized summary stats computation",
       url="https://github.com/predata/white-rabbit",
       author="Predata",
